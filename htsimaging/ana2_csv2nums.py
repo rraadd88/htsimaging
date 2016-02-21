6ddd3f3714cbb47fed8684935b4450d02888ce38
rraadd88 <rraadd_8@hotmail.com>
2016-02-22 00:30:57 +0530

    2 pool

--- htsimaging/ana2_csv2nums.py
+++ htsimaging/ana2_csv2nums.py
@@ -9,7 +9,7 @@ import string
 import numpy as np
 from scipy import stats
 import os
-#from multiprocessing import Pool
+from multiprocessing import Pool
 from os.path import splitext,basename,exists
 
 data_xls_fh=sys.argv[1]
@@ -23,29 +23,22 @@ for var in info.iterrows():
     else:
         exec("%s=info['default']['%s']" % (var[0],var[0]))
 
-def main(data_xls_fh):
-    #data_xls_fh='../test/test.xlsx'
-    data_job=pd.read_excel(data_xls_fh,'JobView')
-    data_job_del_cols=[str(s) for s in data_job.columns if 'Intensity' in str(s) ]
-    for col in data_job_del_cols:
-        exec("del data_job['%s']" % col,locals(), globals())
-    data_job
-    for row in data_job.iterrows():
-        print row[0]
-        row_df=pd.DataFrame(row[1])
-        fn_nd=str(row_df.loc['File Name',row[0]])
-        framei=row_df.loc['File Frame Index',row[0]]
-        fh="%s/%s%02d.csv" % (nd_dh,fn_nd,framei)
-        if exists(fh):
-            arr=np.genfromtxt(fh,delimiter=',')
-            vec=np.concatenate(arr)
-            data_job.loc[row[0],'mode']=stats.mode(vec)[0][0]
-            data_job.loc[row[0],'mean']=np.mean(vec)
-            data_job.loc[row[0],'median']=np.median(vec)
-        else:
-            print ">>> ERROR   : file no not exist : %s" % fh
-        del row_df
-    data_job.to_csv('data_xls_fh_nums.csv')
+def csv2nums(nd_fns_frameis_tps):
+    nd_fn=nd_fns_frameis_tps[0]
+    framei=nd_fns_frameis_tps[1]
+    fh="%s/%s%02d.csv" % (nd_dh,nd_fn,framei)
+    if exists(fh):
+        print ">>> STATUS  : processing : %s" % fh 
+        arr=np.genfromtxt(fh,delimiter=',')
+        vec=np.concatenate(arr)
+        nums=pd.DataFrame()
+        nums.loc[0,'mode']=stats.mode(vec)[0][0]
+        nums.loc[0,'mean']=np.mean(vec)
+        nums.loc[0,'median']=np.median(vec)
+        nums.to_csv(fh+".nums")
+    else:
+        print ">>> ERROR   : file no not exist : %s" % fh
+#     data_job.to_csv('data_xls_fh_nums.csv')
 
 # import matplotlib.pyplot as plt
 # hist=plt.hist(vec,80)
@@ -58,4 +51,17 @@ if __name__ == '__main__':
     if not exists(sys.argv[1]):
         print >> sys.stderr, "### ERROR : Could not find '%s'!\n" % sys.argv[1]
         exit(1)
-    main(sys.argv[1])
+        
+    data_job=pd.read_excel(data_xls_fh,'JobView')
+    data_job_del_cols=[str(s) for s in data_job.columns if 'Intensity' in str(s) ]
+    for col in data_job_del_cols:
+        exec("del data_job['%s']" % col,locals(), globals())
+    nd_fns =[str(s) for s in data_job['File Name'].tolist()]
+    frameis=[int(s) for s in data_job['File Frame Index'].tolist()]
+    nd_fns_frameis_tps=[]
+    for i in range(len(nd_fns)):
+        nd_fns_frameis_tps.append([nd_fns[i],frameis[i]])
+#     csv2nums(nd_fns_frameis_tps[0])
+    pool=Pool(processes=int(cores)) 
+    pool.map(csv2nums,nd_fns_frameis_tps)
+    pool.close(); pool.join()
