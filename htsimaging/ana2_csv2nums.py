a06a5802241eafb1194ef9cf0341505996b99e7b
rraadd88 <rraadd_8@hotmail.com>
2016-02-21 19:08:55 +0530

    2 m,m,m

--- htsimaging/ana2_csv2nums.py
+++ htsimaging/ana2_csv2nums.py
@@ -3,6 +3,7 @@
 # Copyright 2016, Rohan Dandage <rraadd_8@hotmail.com>
 # This program is distributed under General Public License v. 3.    
 
+import sys
 import pandas as pd
 import string
 import numpy as np
@@ -11,7 +12,16 @@ import os
 #from multiprocessing import Pool
 from os.path import splitext,basename,exists
 
-#nd_dh="/media/Transcend/20160219_000356_267"
+data_xls_fh=sys.argv[1]
+info=pd.read_excel(data_xls_fh,'info')
+info=info.set_index('varname')
+for var in info.iterrows():
+    val=info['input'][var[0]]
+    #print var[0]
+    if not pd.isnull(val):
+        exec("%s=info['input']['%s']" % (var[0],var[0]))
+    else:
+        exec("%s=info['default']['%s']" % (var[0],var[0]))
 
 def main(data_xls_fh):
     #data_xls_fh='../test/test.xlsx'
@@ -30,22 +40,22 @@ def main(data_xls_fh):
             arr=np.genfromtxt(fh,delimiter=',')
             vec=np.concatenate(arr)
             data_job.loc[row[0],'mode']=stats.mode(vec)[0][0]
+            data_job.loc[row[0],'mean']=np.mean(vec)
+            data_job.loc[row[0],'median']=np.median(vec)
         else:
             print ">>> ERROR   : file no not exist : %s" % fh
         del row_df
+    data_job.to_csv('data_xls_fh_nums.csv')
 
-import matplotlib.pyplot as plt
-hist=plt.hist(vec,80)
-
-
-
+# import matplotlib.pyplot as plt
+# hist=plt.hist(vec,80)
 
 if __name__ == '__main__':
     # # GET INPTS
-    if 1 < len(sys.argv) < 1: 
-	print "### ERROR : check number of nput args required"
-	sys.exit()
-    if not exists(sys.argv[1]) :
+    if 1 < len(sys.argv) < 1:
+        print "### ERROR : check number of nput args required"
+        sys.exit()
+    if not exists(sys.argv[1]):
         print >> sys.stderr, "### ERROR : Could not find '%s'!\n" % sys.argv[1]
         exit(1)
     main(sys.argv[1])
