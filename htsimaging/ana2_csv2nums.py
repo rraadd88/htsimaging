9baf9a154ee61ebad66f52e8a2cb2509f3abf231
rraadd88 <rraadd_8@hotmail.com>
2016-02-22 01:54:34 +0530

    2 .nums2df

--- htsimaging/ana2_csv2nums.py
+++ htsimaging/ana2_csv2nums.py
@@ -12,32 +12,19 @@ import os
 from multiprocessing import Pool
 from os.path import splitext,basename,exists
 
-data_xls_fh=sys.argv[1]
-info=pd.read_excel(data_xls_fh,'info')
-info=info.set_index('varname')
-for var in info.iterrows():
-    val=info['input'][var[0]]
-    #print var[0]
-    if not pd.isnull(val):
-        exec("%s=info['input']['%s']" % (var[0],var[0]))
-    else:
-        exec("%s=info['default']['%s']" % (var[0],var[0]))
-
-def csv2nums(nd_fns_frameis_tps):
-    nd_fn=nd_fns_frameis_tps[0]
-    framei=nd_fns_frameis_tps[1]
-    fh="%s/%s%02d.csv" % (nd_dh,nd_fn,framei)
-    if exists(fh):
-        print ">>> STATUS  : processing : %s" % fh 
-        arr=np.genfromtxt(fh,delimiter=',')
+def csv2nums(csv_fh):
+    nums_fh=csv_fh+".nums"    
+    if exists(csv_fh) and (not exists(nums_fh)) :
+        print ">>> STATUS  : processing : %s" % csv_fh 
+        arr=np.genfromtxt(csv_fh,delimiter=',')
         vec=np.concatenate(arr)
         nums=pd.DataFrame()
         nums.loc[0,'mode']=stats.mode(vec)[0][0]
         nums.loc[0,'mean']=np.mean(vec)
         nums.loc[0,'median']=np.median(vec)
-        nums.to_csv(fh+".nums")
+        nums.to_csv(nums_fh)
     else:
-        print ">>> ERROR   : file no not exist : %s" % fh
+        print ">>> WARNING   : skipping : %s" % csv_fh
 #     data_job.to_csv('data_xls_fh_nums.csv')
 
 # import matplotlib.pyplot as plt
@@ -51,17 +38,52 @@ if __name__ == '__main__':
     if not exists(sys.argv[1]):
         print >> sys.stderr, "### ERROR : Could not find '%s'!\n" % sys.argv[1]
         exit(1)
+
+    data_xls_fh=sys.argv[1]
+    info=pd.read_excel(data_xls_fh,'info')
+    info=info.set_index('varname')
+    for var in info.iterrows():
+        val=info['input'][var[0]]
+        #print var[0]
+        if not pd.isnull(val):
+            exec("%s=info['input']['%s']" % (var[0],var[0]))
+        else:
+            exec("%s=info['default']['%s']" % (var[0],var[0]))
         
     data_job=pd.read_excel(data_xls_fh,'JobView')
     data_job_del_cols=[str(s) for s in data_job.columns if 'Intensity' in str(s) ]
     for col in data_job_del_cols:
         exec("del data_job['%s']" % col,locals(), globals())
+    
     nd_fns =[str(s) for s in data_job['File Name'].tolist()]
     frameis=[int(s) for s in data_job['File Frame Index'].tolist()]
-    nd_fns_frameis_tps=[]
-    for i in range(len(nd_fns)):
-        nd_fns_frameis_tps.append([nd_fns[i],frameis[i]])
-#     csv2nums(nd_fns_frameis_tps[0])
+    
+    csv_fhs =[]
+    nums_fhs=[]
+    nd_fn_framei_tps=[]
+    for i in range(len(csv_fhs)):
+        csv_fhs.append("%s/%s%02d.csv" % (nd_dh,nd_fns[i],frameis[i]))
+        nums_fhs.append("%s/%s%02d.csv.nums" % (nd_dh,nd_fns[i],frameis[i]))
+        nd_fn_framei_tps.append([nd_fns[i],frameis[i]])
+
+        #       csv2nums(nd_fns_frameis_tps[0])
     pool=Pool(processes=int(cores)) 
-    pool.map(csv2nums,nd_fns_frameis_tps)
+    pool.map(csv2nums,csv_fhs)
     pool.close(); pool.join()
+    
+    data_job=data_job.set_index(['File Name', 'File Frame Index'], inplace=True)
+    for nd_fn_framei_tps in nd_fn_framei_tps:        
+        nd_fn=nd_fn_framei_tps[0]
+        framei=nd_fn_framei_tps[1]
+        data_job[nd_fn][framei]
+        
+    data_job=data_job.reset_index()
+        
+        
+        
+        
+        
+        
+        
+        
+        
