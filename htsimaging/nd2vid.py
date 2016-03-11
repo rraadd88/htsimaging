54d8992df92952b19ea9e64baea0b67b64c556c0
rraadd88 <rraadd_8@hotmail.com>
2016-03-11 17:39:34 +0530

    nd2vid with kin done

--- htsimaging/nd2vid.py
+++ htsimaging/nd2vid.py
@@ -56,7 +56,7 @@ def main(fh_xls,well):
             stb_arr_list.append(stable_image_clipped)
         return stb_arr_list
 
-    def arr_list2regions(arr_list):
+    def arr_list2regions(arr_list, time_increment):
         pre_bleach=arr_list_stb[0]
         smoothened = filters.median(pre_bleach.astype('uint16'),np.ones((4,4)))
         markers = np.zeros(smoothened.shape, dtype=np.uint)
@@ -66,12 +66,23 @@ def main(fh_xls,well):
         regions= measure.label(labels)
         label_objects, nb_labels = ndimage.label(regions)
         sizes = np.bincount(label_objects.ravel())
-        mask_sizes = sizes > 100
+        mask_sizes = ((sizes > 200) & (sizes < 5000))
         mask_sizes[0] = 0
         regions_cleaned = mask_sizes[label_objects]
-        return regions_cleaned
+        props = measure.regionprops(regions,arr_list[0]) #ref
+        regions_areas=np.array([prop.area for prop in props])
+        regions_index_large=np.where((regions_areas<5000) & (regions_areas>200))[0]
+        kins_mean=pd.DataFrame(columns=regions_index_large, index=range(len(arr_list)))
+        for i in range(len(arr_list)):
+            props = measure.regionprops(regions,arr_list[i])
+            means=np.array([prop.mean_intensity for prop in props])
+            kins_mean.loc[i,:]=means[regions_index_large]
+            del props
+        kins_mean=kins_mean.loc[:, ~(kins_mean < 2000).any(axis=0)] #stitch
+        kins_mean['time']=np.array(range(len(arr_list)))*time_increment #stitch
+        return regions_cleaned,kins_mean
 
-    def arr_list2vid(regions,arr_list,vid_fh,xpixels, ypixels):
+    def arr_list2vid(arr_list,regions,kins_mean,vid_fh,xpixels, ypixels):
         dpi = 100
         png_dh=os.path.splitext(vid_fh)[0]
         if not os.path.exists(png_dh):
@@ -79,15 +90,20 @@ def main(fh_xls,well):
                 os.makedirs(png_dh)
             except :
                 print ">>> WARNING : race error data_lbl"
-        fig = plt.figure(figsize=(ypixels/dpi, xpixels/dpi), dpi=dpi)
-        ax = plt.Axes(fig, [0., 0., 1, 1])
-        fig.add_axes(ax)
-        ax.set_axis_off()
-        ax.set_aspect('equal')
+        fig = plt.figure(figsize=(ypixels/dpi*2, xpixels/dpi), dpi=dpi)
+        ax_img = plt.subplot(1,2,1)
+        ax_kin = plt.subplot(1,2,2)
+        ax_img.set_axis_off()
+        ax_img.set_aspect('equal')
         for i in range(len(arr_list)):
-            im=ax.imshow(arr_list[i],cmap='gray',animated=True)
-            im=ax.contour(regions, [0.5], linewidths=1.2, colors='r',animated=False)
+            ax_img.imshow(arr_list[i],cmap='gray',animated=True)
+            ax_img.contour(regions, [0.5], linewidths=1.2, colors='r',animated=False)
+            if len(kins_mean.columns)>1:
+                kins_mean.plot(x='time',legend=False,ax=ax_kin)
+            ax_kin.set_xlim([kins_mean.loc[0,'time'],kins_mean.loc[len(kins_mean)-1,'time']])
+            ax_kin.axvline(kins_mean.loc[i,'time'], color='r', linestyle='--',lw=2)
             plt.savefig(png_dh+'/%02d.png' % i)
+            ax_kin.clear()
         bash_command=("ffmpeg -f image2 -r 4 -i "+png_dh+"/%02d.png -vcodec mpeg4 -y "+vid_fh)
         subprocess.Popen(bash_command, shell=True, executable='/bin/bash')
 
@@ -114,9 +130,9 @@ def main(fh_xls,well):
         nd_fns=data_fns[well].dropna().unique()
         arr_list=nd2arr_list(nd_dh,nd_fns)
         arr_list_stb=raw2phasecorr(arr_list)
-        regions=arr_list2regions(arr_list_stb)
-        arr_list2vid(regions,arr_list_stb,('%s.%sstb.mp4' % (fh_xls,well)),384, 384)
-        arr_list2vid(regions,arr_list    ,('%s.%sraw.mp4' % (fh_xls,well)),384, 384)
+        regions,kins_mean=arr_list2regions(arr_list_stb,time_increment)
+        arr_list2vid(arr_list_stb,regions,kins_mean,('%s.%sstb.mp4' % (fh_xls,well)),384, 384)
+        arr_list2vid(arr_list    ,regions,kins_mean,('%s.%sraw.mp4' % (fh_xls,well)),384, 384)
     else:
         print ">>> STATUS  : nd2vid :already done"
 
