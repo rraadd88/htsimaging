86ff93ef3cc26c214981529b38d56062aa694f71
rraadd88 <rraadd_8@hotmail.com>
2016-07-04 13:35:03 +0530

    spt: moduled

---
+++ imaging/ana_spt.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright 2016, Rohan Dandage <rraadd_8@hotmail.com>
+# This program is distributed under General Public License v. 3.    
+
+import logging
+import pandas as pd
+import numpy as np
+logging.basicConfig(format='[%(asctime)s] %(levelname)s\tfrom %(filename)s in %(funcName)s(..): %(message)s',level=logging.DEBUG) # 
+from imaging.lib import spt #.expt2plots,spt.fit_power,spt.fit_line
+
+def main(expt_dh):
+    expt_data_chem_trial=spt.expt2plots(spt.expt_dh2expt_info(expt_dh),expt_dh)
+
+    expt_data=pd.read_csv(expt_dh+"expt.emsd")
+    if expt_data.index.name!='lagt':
+        expt_data=expt_data.set_index('lagt',drop=True)
+
+    if 'Unnamed: 0' in expt_data.columns.tolist():
+        del expt_data['Unnamed: 0']
+
+    parameters=pd.DataFrame(index=expt_data.columns.tolist(), columns=["power law exponent","power law constant","rsquared of power","slope","y intercept","rsquared of line"])
+
+    for col in expt_data.columns.tolist():
+    #     print col
+        parameters.loc[col,"power law exponent"],\
+        parameters.loc[col,"power law constant"], \
+        parameters.loc[col,"rsquared of power"] = \
+        spt.fit_power(np.array(list(expt_data.index))[:60],np.array(list(expt_data.loc[:,col]))[:60])
+
+        parameters.loc[col,"slope"],\
+        parameters.loc[col,"y intercept"], \
+        parameters.loc[col,"rsquared of line"] = \
+        spt.fit_line(np.log10(np.array(list(expt_data.index))[:60]),np.log10(np.array(list(expt_data.loc[:,col]))[:60]))
+    parameters.to_csv("%s/parameters" % expt_dh)
+
+    
+if __name__ == '__main__':
+    main(sys.argv[1])
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+# Copyright 2016, Rohan Dandage <rohan@igib.in,rraadd_8@hotmail.com>
+# This program is distributed under General Public License v. 3.    
+
+
+"""
+========
+setup.py
+========
+
+installs package
+
+USAGE :
+python setup.py install
+
+Or for local installation:
+
+python setup.py install --prefix=/your/local/dir
+
+"""
+
+import sys
+try:
+    from setuptools import setup, find_packages, Extension 
+except ImportError:
+    from distutils.core import setup, find_packages, Extension
+
+
+if (sys.version_info[0], sys.version_info[1]) != (2, 7):
+    raise RuntimeError('Python 2.7 required ')
+               
+# main setup command
+setup(
+name='imaging',
+author='Rohan Dandage',
+author_email='rraadd_8@hotmail.com,rohan@igib.in',
+version='1.0',
+url='https://github.com/..',
+download_url='https://github.com/rraadd88/..',
+description='..',
+long_description='https://github.com/rraadd88/../README.md',
+license='General Public License v. 3',
+install_requires=[  'pandas >= 0.18.0',
+                    'scipy >= 0.17.0',
+                    'scikit_learn >= 0.17.1',
+                    'matplotlib >= 1.5.1',
+                    'seaborn >= 0.6.0',
+                    'pims == 0.3.3',
+                    # 'opencv == 2.4.9',
+                    # 'scikits.image == 0.11.3'
+                    ],
+platforms='Tested on Linux (Ubuntu 12.04)',
+keywords=['1','1','1'],
+packages=find_packages(),
+)
