1359519ee32011c7fde2d443e44bc113b2998540
rraadd88 <rraadd_8@hotmail.com>
2017-07-11 20:45:25 +0530

    fix: lib added

---
+++ htsimaging/lib/spt.py
@@ -0,0 +1,210 @@
+from os.path import splitext, join, exists, isdir,basename,abspath,dirname
+from os import makedirs
+import trackpy as tp
+# import nd2reader
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pims
+import pims_nd2
+from glob import glob
+import logging
+import pandas as pd
+# %matplotlib inline
+
+@pims.pipeline
+def average_z(image):
+    return image.mean(axis=0) 
+    
+def plot_msd(imsd,emsd,ax,scale="log"):
+    imsd.reset_index().plot(x="lag time [s]",legend=None,alpha=0.75,ax=ax)
+    ax.set(ylabel=r'$\langle \Delta r^2 \rangle$ [$\mu$m$^2$]',
+           xlabel='lag time $t$')
+    if scale=="log":
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+
+    emsd.plot(x="lagt",style='o',legend=None,ax=ax)
+    if scale=="log":
+	    ax.set_xscale('log')
+	    ax.set_yscale('log')
+    ax.set(ylabel=r'$\langle \Delta r^2 \rangle$ [$\mu$m$^2$]',
+           xlabel='lag time $t$')
+    plt.tight_layout()
+    return ax
+
+def plot_emsd(expt_data,ax_emsd,color='k',scale="log"):
+    expt_data.plot(x="lagt",style='o',c=color,ax=ax_emsd,markeredgecolor = 'none')
+    if scale=="log":
+	    ax_emsd.set_xscale('log')
+	    ax_emsd.set_yscale('log')
+    ax_emsd.set(ylabel=r'$\langle \Delta r^2 \rangle$ [$\mu$m$^2$]',
+                xlabel='lag time $t$')
+    ax_emsd.legend(loc = 'center left', bbox_to_anchor = (1.0, 0.5)) #bbox_to_anchor=(2.2, 1.0)
+    plt.tight_layout()
+    return ax_emsd 
+
+def nd2msd(nd_fh):
+    frames=pims.ND2_Reader(nd_fh)
+    if len(np.shape(frames))==4:
+        frames = average_z(frames)
+    f_batch = tp.batch(frames,diameter=11,threshold=np.percentile(frames,75))
+
+    t = tp.link_df(f_batch, search_range=11, memory=3)
+    t_flt = tp.filter_stubs(t, 3*int(len(frames)/4))
+    d = tp.compute_drift(t_flt)
+    t_cor = tp.subtract_drift(t_flt, d)
+    imsd=tp.imsd(t_cor,mpp=0.0645,fps=0.2, max_lagtime=100, statistic='msd')
+    emsd=tp.emsd(t_cor,mpp=0.0645,fps=0.2, max_lagtime=100)
+    return imsd,emsd
+
+def expt_dh2expt_info(expt_dh):
+    try:
+        expt_info=pd.read_csv(expt_dh+"info")
+    except:
+        expt_info=pd.read_csv(expt_dh+"info.csv")
+    if "Unnamed" in expt_info.columns.tolist():
+        cols_del.append(col)
+
+    expt_info2=expt_info.set_index("smpn",drop=True).T
+    expt_info2.columns=expt_info.loc[:,"smpn"]
+    row_2_keep=[]
+    for row in list(expt_info2.index):
+        if "replicate" in row:
+            row_2_keep.append(row)
+
+    expt_info2=expt_info2.loc[row_2_keep,:]
+    if len(expt_info2.index) != len(set(expt_info2.index)):
+        print "duplicate values in index"
+    for col in expt_info2.columns.tolist():
+        for i in range(len(expt_info2)):
+            if not pd.isnull(expt_info2.loc["replicate %d" % (i+1),col]):
+                expt_info2.loc[("replicate %d" % (i+1)),col]=['replicate %d' % (i+1),expt_info2.loc[("replicate %d" % (i+1)),col]]
+            else:
+                expt_info2.loc[("replicate %d" % (i+1)),col]=[np.nan,np.nan]
+    expt_info2=expt_info2.reset_index()
+    del expt_info2["index"]
+    return expt_info2
+
+def expt2plots(expt_info,expt_dh):
+    if not exists(expt_dh):
+        makedirs(expt_dh)
+    expt_data=pd.DataFrame()
+    for test in expt_info:
+        test_info=expt_info.loc[:,test]
+        test_data=pd.DataFrame()    
+        for rep in test_info:
+            if not pd.isnull(rep[0]):
+                repn="%s %s" % (test,rep[0])
+    #             print repn
+                nd_fh=rep[1]
+                if exists(nd_fh):
+                    out_fh="%s/%s" % (expt_dh,repn.replace(" ","_"))
+    #                 print out_fh
+                    plot_fh=out_fh+".imsd.pdf"
+                    if not exists(plot_fh):
+                        try:
+                            imsd,emsd=nd2msd(nd_fh)
+                        except:
+                            continue
+                        emsd=pd.DataFrame(emsd)
+                        print repn
+                        emsd.columns=[repn]
+                        emsd=emsd.reset_index()
+                        imsd.to_csv(out_fh+".imsd",index=False)
+                        emsd.to_csv(out_fh+".emsd",index=False)
+                        fig = plt.figure(figsize=(3, 3))
+                        ax_imsd=plt.subplot(111)
+                        ax_imsd=plot_msd(imsd,emsd,ax_imsd)
+                        ax_imsd.figure.savefig(plot_fh,format='pdf');plt.clf();plt.close()
+                    else:
+                        emsd=pd.read_csv(out_fh+".emsd")
+                        if "Unnamed: 0" in emsd.columns.tolist(): 
+                            del emsd["Unnamed: 0"]
+                else:
+                    print "can not find"
+                if len(test_data)==0:
+                    test_data=emsd
+                else:
+        #                 test_data[repn]=emsd[repn]
+                    test_data=pd.concat([test_data,emsd[repn]],axis=1)
+            test_data.to_csv(expt_dh+test+".emsd",index=False)
+        if len(expt_data)==0:
+            expt_data=test_data
+        else:
+    #             expt_data.loc[:,test_data.columns.tolist()]=test_data.loc[:,test_data.columns.tolist()]
+            expt_data=pd.concat([expt_data,test_data.loc[:,[col for col in test_data.columns.tolist() if col != "lagt"]]],axis=1)
+    #     expt_data=expt_data.drop_duplicates("lagt",keep='first')
+    expt_data.to_csv(expt_dh+"expt.emsd",index=False)
+    fig = plt.figure(figsize=(6, 3))
+    ax_emsd=plt.subplot(121)
+    ax_emsd=plot_emsd(expt_data,ax_emsd)
+    plot_fh=expt_dh+"emsd.pdf"
+    plt.savefig(plot_fh,format='pdf');plt.clf();plt.close()
+    expt_data_log10=np.log10(expt_data)
+    expt_data_log10.to_csv(expt_dh+"expt.emsdlog10")
+    expt_data=expt_data.set_index('lagt',drop=True)
+    return expt_data
+
+
+def createinfo(expt_dh):
+    try:
+        info=pd.read_csv(expt_dh+"info")
+    except:
+        info=pd.read_csv(expt_dh+"info.csv")
+    for i in range(len(info)):
+        reps=glob("%s/%s*" % (info.loc[i,"dh"],info.loc[i,"fn_lead"]))
+        for repi,rep in enumerate(reps):
+            info.loc[i,"replicate %d" % (repi+1)]=rep
+    #     break
+    info.to_csv(expt_dh+"info",index=False)
+    
+    
+from scipy.optimize import leastsq
+
+def power(x, A, B):
+    """power law equation."""
+    return B*x**A
+def power_residuals(p, y, x):
+    """Deviations of data from fitted 4PL curve"""
+    A,B = p
+    err = y-power(x, A, B)
+    return err
+def power_peval(x, p):
+    """Evaluated value at x with current parameters."""
+    A,B = p
+    return power(x, A, B)
+def fit_power(x,y,p0=[0, 1],plot=False):
+    plsq,cov,infodict,mesg,ier= leastsq(power_residuals, p0, args=(y, x),full_output=1)
+    ss_err=(infodict['fvec']**2).sum()
+    ss_tot=((y-y.mean())**2).sum()
+    rsquared=1-(ss_err/ss_tot)
+    if rsquared<0.85:
+        print "poor fit %d" % rsquared
+    if plot==True:
+        plt.plot(x,y,'o')  
+        plt.plot(x,power_peval(x,plsq)) 
+    return plsq[0], plsq[1],rsquared,power_peval(np.max(x),plsq)
+def line(x, m, C):
+    """power law equation."""
+    return m*x+C
+def line_residuals(p, y, x):
+    """Deviations of data from fitted 4PL curve"""
+    m,C = p
+    err = y-line(x, m,C)
+    return err
+def line_peval(x, p):
+    """Evaluated value at x with current parameters."""
+    m,C = p
+    return line(x, m,C)
+def fit_line(x,y,p0=[0, 1],plot=False):
+    plsq,cov,infodict,mesg,ier= leastsq(line_residuals, p0, args=(y, x),full_output=1)
+    ss_err=(infodict['fvec']**2).sum()
+    ss_tot=((y-y.mean())**2).sum()
+    rsquared=1-(ss_err/ss_tot)
+    if rsquared<0.85:
+        print "poor fit %d" % rsquared
+    if plot==True:
+        plt.plot(x,y,'o')  
+        plt.plot(x,line_peval(x,plsq)) 
+    return plsq[0], plsq[1],rsquared
---
+++ htsimaging/lib/utils.py
@@ -0,0 +1,155 @@
+import sys
+import os
+from os import makedirs
+from os.path import exists,splitext,basename
+from glob import glob
+import pims
+# import nd2reader
+import pandas as pd
+import string
+import numpy as np
+from scipy import stats,ndimage
+from multiprocessing import Pool
+import cv2
+from skimage.segmentation import random_walker
+# from skimage.data import binary_blobs
+from skimage import io,exposure,restoration,filters,morphology,measure
+from scipy import ndimage as ndi
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import subprocess
+
+import trackpy as tp
+from imaging.lib import spt
+
+def filterframes(frames,cutoff=0):
+    frames_cleaned=[]
+    for framei in frames:
+        frames_cleaned.append(filterframe(framei,cutoff))
+    return frames_cleaned
+
+def filterframe(frame,cutoff=0):
+    frame_cleand=np.array(frame)
+    frame_cleand[frame_cleandi<cutoff]=0
+    return frame_cleaned
+
+def filter_regions(img,regions,prop_type='area',mn=0,mx=0,check=False):
+    regions_props       = measure.regionprops(regions.astype(int),img)
+    regions_lbls        = np.array([prop.label for prop in regions_props])
+    if prop_type=='area':
+        regions_props_selected = np.array([prop.area  for prop in regions_props])
+    elif prop_type=='mean_intensity':
+        regions_props_selected = np.array([prop.mean_intensity  for prop in regions_props])
+    elif prop_type=='eccentricity':
+        regions_props_selected = np.array([prop.eccentricity  for prop in regions_props])        
+    elif prop_type=='extent':
+        regions_props_selected = np.array([prop.extent  for prop in regions_props])        
+
+    if not check:
+        regions_filtered_lbls = regions_lbls[np.where((regions_props_selected<mx) & (regions_props_selected>mn))[0]]
+        regions_filtered=np.zeros(regions.shape)
+        for lbli in regions_filtered_lbls:
+            regions_filtered[np.where(regions==lbli)]=lbli
+        return regions_filtered
+    elif check:
+        plt.hist(regions_props_selected)
+
+def smoothen(img):
+    denoised=restoration.denoise_bilateral(img.astype('uint16'), sigma_range=0.01, sigma_spatial=15)
+    smoothened = filters.median(denoised,np.ones((4,4)))
+    return smoothened
+
+def smoothenframes(frames):
+    frames_cleaned=[]
+    for framei in frames:
+        frames_cleaned.append(smoothen(np.array(framei)))
+    return frames_cleaned
+        
+def get_regions(img):
+    denoised=restoration.denoise_bilateral(img.astype('uint16'), sigma_range=0.01, sigma_spatial=15)
+    smoothened = filters.median(denoised,np.ones((4,4)))
+    markers = np.zeros(smoothened.shape, dtype=np.uint)
+    markers[smoothened < filters.threshold_otsu(smoothened)] = 1
+    markers[smoothened > filters.threshold_otsu(smoothened)] = 2
+    labels = random_walker(smoothened, markers, beta=10, mode='bf')
+    regions= measure.label(labels)
+    return regions, denoised, smoothened,markers
+
+def raw2phasecorr(arr_list,clip=0): #cv
+    import cv2
+    cx = 0.0
+    cy = 0.0
+    stb_arr_list=[]
+    prev_frame = arr_list[0]
+    prev_image = np.float32(restoration.denoise_tv_chambolle(prev_frame.astype('uint16'), weight=0.1, multichannel=True)) #ref
+    for frame in arr_list:           
+        image = np.float32(restoration.denoise_tv_chambolle(frame.astype('uint16'), weight=0.1, multichannel=True))
+        # TODO: set window around phase correlation
+        dp = cv2.phaseCorrelate(prev_image, image)
+        cx = cx - dp[0]
+        cy = cy - dp[1]
+        xform = np.float32([[1, 0, cx], [0, 1, cy]])
+        stable_image = cv2.warpAffine(frame.astype('float32'), xform, dsize=(image.shape[1], image.shape[0]))
+        prev_image = image
+        #clip sides
+        ht,wd=np.shape(stable_image)
+#         clip=0.125 #0.25
+        lt=int(wd*clip)
+        rt=int(wd-wd*clip)
+        up=int(ht*clip)
+        dw=int(ht-ht*clip)
+        stable_image_clipped=stable_image[up:dw,lt:rt]
+        stb_arr_list.append(stable_image_clipped)
+    return stb_arr_list
+
+def imlistcropper(imlist,loci):
+    rowini,rowend,colini,colend=loci
+    imlist_region=[]
+    for im in imlist:
+        imlist_region.append(im[rowini:rowend,colini:colend])
+    return imlist_region
+
+
+def imclipper(im_stb,clip):
+    ht,wd=np.shape(im_stb)
+#         clip=0.125 #0.25
+    lt=int(wd*clip)
+    rt=int(wd-wd*clip)
+    up=int(ht*clip)
+    dw=int(ht-ht*clip)
+    im_stb_clipped=im_stb[up:dw,lt:rt]
+    return im_stb_clipped
+
+def phasecorr(imlist,imlist2=None,clip=0): #cv  [rowini,rowend,colini,colend]
+    import cv2
+    cx = 0.0
+    cy = 0.0            
+    imlist_stb=[]
+    if imlist2!=None:
+        imlist2_stb=[]
+
+    imi=0
+    im_prev = imlist[0]
+    im_denoised_prev = np.float32(restoration.denoise_tv_chambolle(im_prev.astype('uint16'), weight=0.1, multichannel=True)) #ref
+    for im in imlist:           
+        im_denoised = np.float32(restoration.denoise_tv_chambolle(im.astype('uint16'), weight=0.1, multichannel=True))
+        # TODO: set window around phase correlation
+        dp = cv2.phaseCorrelate(im_denoised_prev, im_denoised)
+        cx = cx - dp[0]
+        cy = cy - dp[1]
+        xform = np.float32([[1, 0, cx], [0, 1, cy]])
+        im_stb = cv2.warpAffine(im.astype('float32'), xform, dsize=(im_denoised.shape[1], im_denoised.shape[0]))
+        imlist_stb.append(imclipper(im_stb,clip))
+
+        if imlist2!=None:
+            im2=imlist2[imi]
+            im2_stb=cv2.warpAffine(im2.astype('float32'), xform, dsize=(im_denoised.shape[1], im_denoised.shape[0]))
+            imlist2_stb.append(imclipper(im2_stb,clip))
+
+        im_denoised_prev = im_denoised
+        imi+=1
+    if imlist2!=None:
+        return imlist_stb,imlist2_stb
+    else:
+        return imlist_stb
