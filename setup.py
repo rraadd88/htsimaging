c3171c9683a22d9226f00dab951526caecd53a74
rraadd88 <rraadd_8@hotmail.com>
2020-02-26 12:21:52 -0500

    update

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ if (sys.version_info[0], sys.version_info[1]) != (3, 6):
 # with open('requirements.txt','w') as f:
 #     f.write('\n'.join(dropna(yaml.load(open('environment.yml','r'))['dependencies'][2]['pip'])))
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
 
     
 # main setup command
@@ -49,7 +49,7 @@ download_url='https://github.com/rraadd88/htsimaging',
 description='For analysis of microscopy images',
 long_description='https://github.com/rraadd88/htsimaging/README.md',
 license='General Public License v. 3',
-install_requires=required,
+# install_requires=required,
 platforms='Tested on Ubuntu 16.04',
 keywords=['microscopy','imaging','trackpy'],
 packages=find_packages(),    
